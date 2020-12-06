e64b8f478a6eb71db3e63a5f53307e8d96c47bf7
Fred <valhallatechnc@gmail.com>
2020-12-06 08:57:27 -0500

    Major refactor

--- lib/email-js/index.d.ts
+++ lib/email-js/index.d.ts
@@ -1,5 +1,7 @@
 declare module "email-js" {
-  export function isValidEmail(email: string): boolean
-  export function getLocalPart(email: string): string
-  export function getDomainPart(email: string): string
+    export function isValidEmail ( email: string ): boolean;
+
+    export function getLocalPart ( email: string ): string;
+
+    export function getDomainPart ( email: string ): string;
 }
